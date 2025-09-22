28ce2f1ea7cbb86575d0d99c03a73fe5d2d1b6ff
charlesojihoji <charlesojihoji@gmail.com>
2025-09-22 13:38:52 -0400

    Added mostly more methods to the ContactControllers

--- contact/src/main/java/edu/uoengland/contact/controllers/ContactControllers.java
+++ contact/src/main/java/edu/uoengland/contact/controllers/ContactControllers.java
@@ -1,8 +1,15 @@
 package edu.uoengland.contact.controllers;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,11 +27,36 @@ public class ContactControllers {
 	private ContactService contactService;
 	
 	@PostMapping
-	public String saveAMessage(@RequestBody ContactDetailsDTO contactDetailsDTO) {
+	public String saveAContact(@RequestBody ContactDetailsDTO contactDetailsDTO) {
 		
-		contactService.saveAMessage(contactDetailsDTO);
+		contactService.saveAContact(contactDetailsDTO);
 		
 		return "The message has been successfully saved.";
 	}
 	
+	@GetMapping
+	public List<ContactDetailsDTO> getAllContacts() {
+		
+		return contactService.getAllContacts();
+	}
+	
+	@GetMapping("/{contactId}")
+	public ContactDetailsDTO getAContact(@PathVariable UUID contactId) {
+		
+		return contactService.getAContact(contactId);
+	}
+	
+	@PutMapping("/updateAContact")
+	public String updateAContact(@RequestBody ContactDetailsDTO contactDetailsDTO) {
+		
+		contactService.updateAContact(contactDetailsDTO);
+		
+		return "The contact has been successfully updated.";
+	}
+	
+	@DeleteMapping("/{contactId}")
+	public String deleteAContact(@PathVariable UUID contactId) {
+		
+		return contactService.deleteAContact(contactId);
+	}
 }
--- contact/src/main/java/edu/uoengland/contact/service/ContactServiceImpl.java
+++ contact/src/main/java/edu/uoengland/contact/service/ContactServiceImpl.java
@@ -1,5 +1,10 @@
 package edu.uoengland.contact.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,20 +13,80 @@ import edu.uoengland.contact.entity.ContactDetails;
 import edu.uoengland.contact.repository.ContactRepository;
 
 @Service
-public class ContactServiceImpl implements ContactService{
+public class ContactServiceImpl implements ContactService {
 
 	@Autowired
 	private ContactRepository contactRepository;
-	
-	@Override
-	public String saveAMessage(ContactDetailsDTO contactDetailsDTO) {
 
-		ContactDetails theContactDetails = new ContactDetails(contactDetailsDTO.getContactId(), contactDetailsDTO.getContactName(), contactDetailsDTO.getPhoneNumber(),
-				contactDetailsDTO.getAddress(), contactDetailsDTO.getEmailAddress(), contactDetailsDTO.getMessageContent());
-		
+	@Override
+	public String saveAContact(ContactDetailsDTO contactDetailsDTO) {
+
+		ContactDetails theContactDetails = new ContactDetails(contactDetailsDTO.getContactId(),
+				contactDetailsDTO.getContactName(), contactDetailsDTO.getPhoneNumber(), contactDetailsDTO.getAddress(),
+				contactDetailsDTO.getEmailAddress(), contactDetailsDTO.getMessageContent());
+
 		contactRepository.save(theContactDetails);
-		
+
 		return "The message has successfully been saved in the database.";
+	}
+
+	@Override
+	public List<ContactDetailsDTO> getAllContacts() {
+
+		List<ContactDetails> theList = contactRepository.findAll();
+
+		List<ContactDetailsDTO> theDTOList = new ArrayList<>();
+
+		for (ContactDetails ctdetails : theList) {
+
+			ContactDetailsDTO cntDetailsDTO = new ContactDetailsDTO(ctdetails.getContactId(),
+					ctdetails.getContactName(), ctdetails.getPhoneNumber(), ctdetails.getAddress(),
+					ctdetails.getEmailAddress(), ctdetails.getMessageContent());
+
+			theDTOList.add(cntDetailsDTO);
+		}
+
+		return theDTOList;
+	}
+
+
+	@Override
+	public ContactDetailsDTO getAContact(UUID contactId) {
+
+		Optional<ContactDetails> contactDetails = contactRepository.findById(contactId);
+		
+		ContactDetailsDTO cntDetailsDTO = new ContactDetailsDTO(contactDetails.get().getContactId(), contactDetails.get().getContactName(),
+				contactDetails.get().getPhoneNumber(), contactDetails.get().getAddress(), contactDetails.get().getEmailAddress(),
+				contactDetails.get().getMessageContent());
+
+		return cntDetailsDTO;
+	}
+
+	@Override
+	public String deleteAContact(UUID contactId) {
+
+		contactRepository.deleteById(contactId);
+
+		return "That contact has been deleted.";
+	}
+
+	@Override
+	public String updateAContact(ContactDetailsDTO contactDetailsDTO) {
+
+		Optional<ContactDetails> contactDetails = contactRepository.findById(contactDetailsDTO.getContactId());
+		
+		ContactDetails contactDetailsUpdated = contactDetails.get();
+		
+		contactDetailsUpdated.setContactId(contactDetailsDTO.getContactId());
+		contactDetailsUpdated.setContactName(contactDetailsDTO.getContactName());
+		contactDetailsUpdated.setPhoneNumber(contactDetailsDTO.getPhoneNumber());
+		contactDetailsUpdated.setAddress(contactDetailsDTO.getAddress());
+		contactDetailsUpdated.setEmailAddress(contactDetailsDTO.getEmailAddress());
+		contactDetailsUpdated.setMessageContent(contactDetailsDTO.getMessageContent());
+		
+		contactRepository.save(contactDetailsUpdated);
+		
+		return "The contact has successfully been updated.";
 	}
 
 }
